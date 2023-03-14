f4d678ca855d81163ae556d3b9db1c1e5f50f6c0
zhengzongwei <zhengzongwei@foxmail.com>
2023-03-14 14:31:10 +0800

    Revert "fe(mariadb): support deploy shell"

--- mariadb/run.sh
+++ mariadb/run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-MARIADB_PATH=$HOME/mariadb/data
+mkdir -p $HOME/mariadb/data
 
-sed -i '/^MARIADB_PATH/d' .env
-
-echo "MARIADB_PATH=$MARIADB_PATH" >> .env
-
-mkdir -p $MARIADB_PATH
-
-docker-compose -f mariadb.yaml up -d
+docker-compose -f mariadb.yaml up -d 
