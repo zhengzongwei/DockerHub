244368fa846ed62ac7e9ede05fcfe4382bbddb3d
zhengzongwei <zhengzongwei@foxmail.com>
2023-03-14 14:28:29 +0800

    fe(mariadb): support deploy shell

--- mariadb/run.sh
+++ mariadb/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-mkdir -p $HOME/mariadb/data
+MARIADB_PATH=$HOME/mariadb/data
 
-docker-compose -f mariadb.yaml up -d 
+sed -i '/^MARIADB_PATH/d' .env
+
+echo "MARIADB_PATH=$MARIADB_PATH" >> .env
+
+mkdir -p $MARIADB_PATH
+
+docker-compose -f mariadb.yaml up -d
